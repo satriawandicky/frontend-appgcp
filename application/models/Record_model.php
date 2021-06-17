da2a786efc95b10bea9a6ef3d51a561b54f3b15d
Dicky Satriawan <dicky.satriawan8@gmail.com>
2021-06-17 12:15:34 +0700

    update base url

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "[API BASE URL]",
+            'base_uri' => "http://34.101.113.249:8000/",
         ]);
     }
 
